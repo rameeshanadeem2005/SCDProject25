5ec09f365d81716edac9d4b6c6d724a279115a2c
rameeshanadeem2005 <nadeemrameesha510@gmail.com>
2025-11-26 23:18:51 +0000

    updated main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const connectMongo = require("./db/mongo");
 const fs =require('fs');
 const path = require('path');
 const readline = require('readline');
@@ -9,10 +10,9 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-function searchRecords(){
 
+connectMongo();
 
-}
 function menu() {
   console.log(`
 ===== NodeVault =====
